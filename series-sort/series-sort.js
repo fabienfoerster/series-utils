cdaa83398a3979377cfcac592e9d672018c2a991
Fabien Foerster <fabienfoerster@gmail.com>
2017-06-18 17:43:56 +0200

    add normalize function for serie name

--- series-sort/series-sort.js
+++ series-sort/series-sort.js
@@ -1,34 +1,44 @@
 const fs = require('fs')
 const path = require('path')
 
-const patterns = [/(.*?)[. -][Ss]([0-9]{1,2})[Ee]([0-9]{1,2})(.*)/, /(.*?)[.]([0-9]{1,2})([0-9]{2})(.*)/, /(.*?)[.]?([0-9]{1,2})x([0-9]{1,2})(.*)/];
-const extensions = [".mp4", ".avi", ".mkv", ".srt"];
+const patterns = [
+  /(.*?)[. -_]?(?:20\d{2})?[. -_]?[Ss]([0-9]{1,2})[Ee]([0-9]{1,2})(.*)/,
+  /(.*?)[. -_]([0-9]{1,2})([0-9]{2})(.*?)/,
+  /(.*?)[. -_]?([0-9]{1,2})x([0-9]{1,2})(.*?)/
+]
+const extensions = ['.mp4', '.avi', '.mkv', '.srt']
 
 function isExtensionOK(ext) {
-    return extensions.includes(ext);
+  return extensions.includes(ext)
 }
 
 function isSampleFile(filename) {
-    const regex = /sample/
-    return regex.test(filename)
+  const regex = /sample/
+  return regex.test(filename)
 }
 
 function ensureTwoDigitSeason(season) {
-    return season.length < 2 ? "0"+season : season
+  return season.length < 2 ? '0' + season : season
+}
+
+function normalize(name) {
+  return name.replace(/-/g, '.').replace(/ /g, '.').replace(/_/g, '.')
 }
 
 function newSeriePath(serie) {
-    let filename = path.basename(serie)
-    for (let i = 0 ; i < patterns.length; i ++) {
-        const re = new RegExp(patterns[i])
-        if (re.test(filename)) {
-            const [, name, season, episode, version] = re.exec(filename)
-            return `${name}/season${ensureTwoDigitSeason(season)}/${filename}`.toLowerCase()
-        }
+  let filename = normalize(path.basename(serie))
+  for (let i = 0; i < patterns.length; i++) {
+    const re = new RegExp(patterns[i])
+    if (re.test(filename)) {
+      const [, name, season, episode, version] = re.exec(filename)
+      return `${name}/season${ensureTwoDigitSeason(
+        season
+      )}/${filename}`.toLowerCase()
     }
-    return `1#ATrier/${filename}`
+  }
+  return `1#ATrier/${filename}`
 }
 
-exports.isExtensionOK = isExtensionOK;
-exports.isSampleFile = isSampleFile;
-exports.newSeriePath = newSeriePath;
+exports.isExtensionOK = isExtensionOK
+exports.isSampleFile = isSampleFile
+exports.newSeriePath = newSeriePath
--- series-sort/series-sort.test.js
+++ series-sort/series-sort.test.js
@@ -1,62 +1,90 @@
-const seriesSort = require('./series-sort');
-const isExtensionOK = seriesSort.isExtensionOK;
-const isSampleFile = seriesSort.isSampleFile;
-const newSeriePath = seriesSort.newSeriePath;
+const seriesSort = require('./series-sort')
+const isExtensionOK = seriesSort.isExtensionOK
+const isSampleFile = seriesSort.isSampleFile
+const newSeriePath = seriesSort.newSeriePath
 
-
-test("extension .mp4 should be OK",() => {
-    expect(isExtensionOK(".mp4")).toBe(true)
+test('extension .mp4 should be OK', () => {
+  expect(isExtensionOK('.mp4')).toBe(true)
 })
 
-test("extension .avi should be OK",() => {
-    expect(isExtensionOK(".avi")).toBe(true)
+test('extension .avi should be OK', () => {
+  expect(isExtensionOK('.avi')).toBe(true)
 })
 
-test("extension .mkv should be OK",() => {
-    expect(isExtensionOK(".mkv")).toBe(true)
+test('extension .mkv should be OK', () => {
+  expect(isExtensionOK('.mkv')).toBe(true)
 })
 
-test("extension .srt should be OK",() => {
-    expect(isExtensionOK(".srt")).toBe(true)
+test('extension .srt should be OK', () => {
+  expect(isExtensionOK('.srt')).toBe(true)
 })
 
-test("extension .txt should be KO",() => {
-    expect(isExtensionOK(".txt")).toBe(false)
+test('extension .txt should be KO', () => {
+  expect(isExtensionOK('.txt')).toBe(false)
 })
 
-test("sample file shoud be detected", () => {
-    expect(isSampleFile("2.broke.girls.417.hdtv-lol.sample.mp4")).toBe(true)
+test('sample file shoud be detected', () => {
+  expect(isSampleFile('2.broke.girls.417.hdtv-lol.sample.mp4')).toBe(true)
 })
 
-test("fleche.4x02.IMMERSIF.mp4", () => {
-    expect(newSeriePath("fleche.4x02.IMMERSIF.mp4")).toBe("fleche/season04/fleche.4x02.immersif.mp4")
+test('fleche.4x02.IMMERSIF.mp4', () => {
+  expect(newSeriePath('fleche.4x02.IMMERSIF.mp4')).toBe(
+    'fleche/season04/fleche.4x02.immersif.mp4'
+  )
 })
 
-test("elementaire.302.IMMERSION.mp4", () => {
-    expect(newSeriePath("elementaire.302.IMMERSION.mp4")).toBe("elementaire/season03/elementaire.302.immersion.mp4")
+test('elementaire.302.IMMERSION.mp4', () => {
+  expect(newSeriePath('elementaire.302.IMMERSION.mp4')).toBe(
+    'elementaire/season03/elementaire.302.immersion.mp4'
+  )
 })
 
-test("Foire.du.Trone.S01E01.IMMERSE.720p.mkv", () => {
-    expect(newSeriePath("Foire.du.Trone.S01E01.IMMERSE.720p.mkv")).toBe("foire.du.trone/season01/foire.du.trone.s01e01.immerse.720p.mkv")
+test('Foire.du.Trone.S01E01.IMMERSE.720p.mkv', () => {
+  expect(newSeriePath('Foire.du.Trone.S01E01.IMMERSE.720p.mkv')).toBe(
+    'foire.du.trone/season01/foire.du.trone.s01e01.immerse.720p.mkv'
+  )
 })
 
-test("marcel.agents.of.p.a.i.n.s03e19.720p.hdtv.x264-killers.[vtv].mkv", () => {
-    expect(newSeriePath("marcel.agents.of.p.a.i.n.s03e19.720p.hdtv.x264-killers.[vtv].mkv")).toBe("marcel.agents.of.p.a.i.n/season03/marcel.agents.of.p.a.i.n.s03e19.720p.hdtv.x264-killers.[vtv].mkv")
+test('marcel.agents.of.p.a.i.n.s03e19.720p.hdtv.x264-killers.[vtv].mkv', () => {
+  expect(
+    newSeriePath(
+      'marcel.agents.of.p.a.i.n.s03e19.720p.hdtv.x264-killers.[vtv].mkv'
+    )
+  ).toBe(
+    'marcel.agents.of.p.a.i.n/season03/marcel.agents.of.p.a.i.n.s03e19.720p.hdtv.x264.killers.[vtv].mkv'
+  )
 })
 
-test("normalnatural.1119.hdtv-lol[ettv].mkv", () => {
-    expect(newSeriePath("normalnatural.1119.hdtv-lol[ettv].mkv")).toBe("normalnatural/season11/normalnatural.1119.hdtv-lol[ettv].mkv")
+test('normalnatural.1119.hdtv-lol[ettv].mkv', () => {
+  expect(newSeriePath('normalnatural.1119.hdtv-lol[ettv].mkv')).toBe(
+    'normalnatural/season11/normalnatural.1119.hdtv.lol[ettv].mkv'
+  )
 })
 
-test("normalnatural.119.hdtv-lol.720p.[ettv].mkv", () => {
-    expect(newSeriePath("normalnatural.119.hdtv-lol.720p.[ettv].mkv")).toBe("normalnatural/season01/normalnatural.119.hdtv-lol.720p.[ettv].mkv")
+test('normalnatural.119.hdtv-lol.720p.[ettv].mkv', () => {
+  expect(newSeriePath('normalnatural.119.hdtv-lol.720p.[ettv].mkv')).toBe(
+    'normalnatural/season01/normalnatural.119.hdtv.lol.720p.[ettv].mkv'
+  )
 })
 
-test("Foire.du.Trone.S06E03.IMMERSIFASF.1080p/Foire.du.Trone.S06E03.IMMERSIFASF.1080p.avi", () => {
-    expect(newSeriePath("Foire.du.Trone.S06E03.IMMERSIFASF.1080p/Foire.du.Trone.S06E03.IMMERSIFASF.1080p.avi")).toBe("foire.du.trone/season06/foire.du.trone.s06e03.immersifasf.1080p.avi")
+test('Foire.du.Trone.S06E03.IMMERSIFASF.1080p/Foire.du.Trone.S06E03.IMMERSIFASF.1080p.avi', () => {
+  expect(
+    newSeriePath(
+      'Foire.du.Trone.S06E03.IMMERSIFASF.1080p/Foire.du.Trone.S06E03.IMMERSIFASF.1080p.avi'
+    )
+  ).toBe('foire.du.trone/season06/foire.du.trone.s06e03.immersifasf.1080p.avi')
 })
 
-test("Splinter Cell Blacklist Shower.mp4", () => {
-    expect(newSeriePath("Splinter Cell Blacklist Shower.mp4")).toBe("1#ATrier/Splinter Cell Blacklist Shower.mp4")
+test('Splinter Cell Blacklist Shower.mp4', () => {
+  expect(newSeriePath('Splinter Cell Blacklist Shower.mp4')).toBe(
+    '1#ATrier/Splinter.Cell.Blacklist.Shower.mp4'
+  )
 })
 
+test('House.Of.Cards.2013.S01E01.720p.BluRay.x264-DEMAND.mkv', () => {
+  expect(
+    newSeriePath('House.Of.Cards.2013.S01E01.720p.BluRay.x264-DEMAND.mkv')
+  ).toBe(
+    'house.of.cards/season01/house.of.cards.2013.s01e01.720p.bluray.x264.demand.mkv'
+  )
+})
